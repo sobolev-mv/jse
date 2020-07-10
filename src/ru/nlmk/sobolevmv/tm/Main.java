4825705a728841611b82b63d4f5dda2e97fe1f95
sobolev_mv <sobolev_mv@nlmk.com>
2020-07-10 12:29:34 +0500

    Реализована поддержка аргументов коммандной строки.

--- src/ru/nlmk/sobolevmv/tm/Main.java
+++ src/ru/nlmk/sobolevmv/tm/Main.java
@@ -1,8 +1,58 @@
 package ru.nlmk.sobolevmv.tm;
 
+import static ru.nlmk.sobolevmv.tm.constant.TerminalConst.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	  System.out.println("Hello World!");
+    public static void main(final String[] args) {
+        displayWelcome();
+        run(args);
     }
+    private static void displayError(){
+      System.out.println("Error! Unknow program argument.");
+      System.exit(-1);
+    }
+
+    private static void displayHelp(){
+      System.out.println("version - Display program version.");
+      System.out.println("about -  Display developer info.");
+      System.out.println("help - Display list of terminal commands.");
+      System.exit(0);
+    }
+
+    private static void displayVersion(){
+      System.out.println("1.0.0");
+      System.exit(0);
+    }
+
+    private static void displayAbout(){
+      System.out.println("Mikhail Sobolev");
+      System.out.println("sobolev_mv@nlmk.com");
+      System.exit(0);
+    }
+
+    private static void displayWelcome(){
+      System.out.println("** WELCOME TO TASK MANAGER **");
+    }
+
+    private static void run(final String[] args){
+      if (args == null) return;
+      if (args.length < 1) return;
+      final String param = args[0];
+      switch(param) {
+          case CMD_HELP:
+              displayHelp();
+          case CMD_VERSION:
+              displayVersion();
+          case CMD_ABOUT:
+              displayAbout();
+          default:
+              displayError();
+      }
+    }
+
+
+
+
+
 }
---
+++ src/ru/nlmk/sobolevmv/tm/constant/TerminalConst.java
@@ -0,0 +1,7 @@
+package ru.nlmk.sobolevmv.tm.constant;
+
+public final class TerminalConst {
+    public static final String CMD_HELP = "help";
+    public static final String CMD_VERSION = "version";
+    public static final String CMD_ABOUT = "about";
+}
