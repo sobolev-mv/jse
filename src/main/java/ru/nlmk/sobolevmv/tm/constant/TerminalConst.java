a78f5c551dc453f560badaf022d9f1f115beacc3
sobolev_mv <sobolev_mv@nlmk.com>
2020-07-20 13:17:17 +0500

    Создан новый проект jse-05

--- src/main/java/ru/nlmk/sobolevmv/tm/constant/TerminalConst.java
+++ src/main/java/ru/nlmk/sobolevmv/tm/constant/TerminalConst.java
@@ -5,4 +5,5 @@ public final class TerminalConst {
     public static final String CMD_VERSION = "version";
     public static final String CMD_ABOUT = "about";
     public static final String CMD_EXIT = "exit";
+    public static final String CMD_QUIT = "quit";
 }
